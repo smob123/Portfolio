b2f1bad39073448791a59f25e33a9a1605b91987
Sultan <smob123@hotmail.com>
2018-12-02 20:00:39 +0300

    enabled contact form

--- src/res/assets/contact.js
+++ src/res/assets/contact.js
@@ -1,16 +1,59 @@
 import React, { Component } from 'react';
+import $ from 'jquery';
 
 export default class Contact extends Component {
+    constructor() {
+        super();
+        this.state = {
+            name: '',
+            email: '',
+            subject: '',
+            message: ''
+        }
+    }
+
+    update = e => {
+        this.setState({
+            [e.target.name]: e.target.value.toString()
+        });
+    }
+
+    submit = e => {
+        e.preventDefault();
+        this.sendData(JSON.stringify(this.state));
+    }
+
+    sendData(data) {
+        fetch('https://m-node-server.herokuapp.com/send-email', {
+            method: 'POST',
+            body: data,
+            headers: {
+                'content-type': 'application/json'
+            }
+        })
+            .then((res) => res.json())
+            .then((json) => {
+                if (json.message !== 'ok') {
+                    alert(json.message);
+                }
+                else {
+                    $('#contact-content form').css({ 'display': 'none' });
+                    $('#contact-content').append('<span>Your message has been submitted!</span>');
+                }
+            })
+            .catch((err) => console.log(err));
+    }
+
     render() {
         return (
-                <div>
-                <input placeholder='Name' />
-                <input placeholder='E-mail' />
-                <input placeholder='Subject' />
-                <textarea placeholder='Message' />
+            <form>
+                <input placeholder='Name' name='name' value={this.state.name} onChange={e => this.update(e)} />
+                <input placeholder='E-mail' type='email' name='email' value={this.state.email} onChange={e => this.update(e)} />
+                <input placeholder='Subject' name='subject' value={this.state.subject} onChange={e => this.update(e)} />
+                <textarea placeholder='Message' name='message' value={this.state.message} onChange={e => this.update(e)} />
 
-                    <button id='message-submit'>Submit</button>
-                </div>
+                <button id='message-submit' onClick={e => this.submit(e)}>Submit</button>
+            </form>
         );
     }
 }
