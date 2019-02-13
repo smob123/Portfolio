8167c35e4dc5d453a2085f489ebea4d373267931
Sultan <smob123@hotmail.com>
2019-02-14 00:23:13 +0300

    Updates

---
+++ precache-manifest.dc1f783ac7c96882b5b062ad8fc60276.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "d6e24accc2c69be95a2913b52089ea2d",
+    "url": "/Portfolio/static/media/rdcsi.d6e24acc.jpg"
+  },
+  {
+    "revision": "8c5930eb103d99b7861f",
+    "url": "/Portfolio/static/css/main.65832716.chunk.css"
+  },
+  {
+    "revision": "2fbcbe02f467b31056bf",
+    "url": "/Portfolio/static/js/1.2fbcbe02.chunk.js"
+  },
+  {
+    "revision": "3b96116072eb85f5a3a2",
+    "url": "/Portfolio/static/js/runtime~main.3b961160.js"
+  },
+  {
+    "revision": "1f0f3724598e4166fad36a2c2173fbe3",
+    "url": "/Portfolio/static/media/JS.1f0f3724.png"
+  },
+  {
+    "revision": "a06de35baf0fe5fec924ce16f1baacda",
+    "url": "/Portfolio/static/media/ReactNative.a06de35b.png"
+  },
+  {
+    "revision": "8648c71ee050c7fe690e42aa97b344f1",
+    "url": "/Portfolio/static/media/header.8648c71e.jpg"
+  },
+  {
+    "revision": "824687bf51ddf785ff738d196f01359f",
+    "url": "/Portfolio/static/media/React.824687bf.png"
+  },
+  {
+    "revision": "bdba4f1ee1508a23e52eb780d6d999cb",
+    "url": "/Portfolio/static/media/Gatsby.bdba4f1e.png"
+  },
+  {
+    "revision": "ba3c2a76f604639b73771b35e8fc2e32",
+    "url": "/Portfolio/static/media/groceristar.ba3c2a76.PNG"
+  },
+  {
+    "revision": "8c5930eb103d99b7861f",
+    "url": "/Portfolio/static/js/main.8c5930eb.chunk.js"
+  },
+  {
+    "revision": "a071b077f73b34503503c6f18975f60a",
+    "url": "/Portfolio/static/media/GraphQL.a071b077.png"
+  },
+  {
+    "revision": "89ec32967c2ed61368841a2f1717d69d",
+    "url": "/Portfolio/static/media/flutter.89ec3296.png"
+  },
+  {
+    "revision": "e8ea8cf667d1ddd05861e9508628a284",
+    "url": "/Portfolio/static/media/CSS3.e8ea8cf6.png"
+  },
+  {
+    "revision": "53ecca5fb46f726ac918ee7caeaf2a7a",
+    "url": "/Portfolio/static/media/RestaurantFinder.53ecca5f.png"
+  },
+  {
+    "revision": "894c06a1e1ae6c2eae3177fe6d97e283",
+    "url": "/Portfolio/static/media/MoviesApp.894c06a1.png"
+  },
+  {
+    "revision": "2878be5f1c34daf5398f8f5ddba6038b",
+    "url": "/Portfolio/static/media/SultansKitchen.2878be5f.PNG"
+  },
+  {
+    "revision": "3eac64ffa1569cbf34a4ed20c7eac092",
+    "url": "/Portfolio/static/media/phone-laptop.3eac64ff.png"
+  },
+  {
+    "revision": "9f6f69696e85b1a93aa3dafc61d0937e",
+    "url": "/Portfolio/static/media/Sourcerer.9f6f6969.svg"
+  },
+  {
+    "revision": "c5d65dfbbaea62adbe0e285524a91ad6",
+    "url": "/Portfolio/index.html"
+  }
+];
