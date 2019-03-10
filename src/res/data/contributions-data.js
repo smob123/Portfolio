0f8ba20fd02fab516d340952d7667958bc6598ee
Sultan <smob123@hotmail.com>
2019-03-10 22:03:34 +1300

    package updates, and project description chnages

--- src/res/data/contributions-data.js
+++ src/res/data/contributions-data.js
@@ -8,7 +8,7 @@ import RDCSI from '../images/rdcsi.jpg';
 let groceristar = {
     logo: Groceristar,
     title: "Groceristar's homepage",
-    description: 'An online grocery list app',
+    description: 'An online grocery list app, in which I contributed to the development of multiple templates; such as, a calendar, a features page, etc...',
     link: 'https://github.com/GroceriStar/onboarding-features',
     technologies: [
         JS,
@@ -21,7 +21,7 @@ let groceristar = {
 let rdcsi = {
     logo: RDCSI,
     title: 'Romeo Dallaire Child Soldiers Initiative',
-    description: 'Hybrid Diet tracking app',
+    description: "Hybrid Diet tracking app, in which I contributed to the implementation of a radar chart that displays the user's activity",
     technologies: [
         JS,
         ReactNative
