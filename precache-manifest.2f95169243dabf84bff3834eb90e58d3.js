e03af2647f1a82c554499eaca792f3ddf1aec1a8
Sultan <smob123@hotmail.com>
2019-05-06 10:03:52 +1200

    Updates

---
+++ precache-manifest.2f95169243dabf84bff3834eb90e58d3.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "792605f737a52f97dab75c26e115cc89",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "dc78ba4d7bd5301ade21",
+    "url": "/Portfolio/static/css/main.bb17ba01.chunk.css"
+  },
+  {
+    "revision": "d5d51201e56fd98b2d9d",
+    "url": "/Portfolio/static/js/2.bafd12d9.chunk.js"
+  },
+  {
+    "revision": "dc78ba4d7bd5301ade21",
+    "url": "/Portfolio/static/js/main.b24fa775.chunk.js"
+  },
+  {
+    "revision": "6b64d540f1308ee928ad",
+    "url": "/Portfolio/static/js/runtime~main.132fbc8c.js"
+  },
+  {
+    "revision": "e8ea8cf667d1ddd05861e9508628a284",
+    "url": "/Portfolio/static/media/CSS3.e8ea8cf6.png"
+  },
+  {
+    "revision": "bdba4f1ee1508a23e52eb780d6d999cb",
+    "url": "/Portfolio/static/media/Gatsby.bdba4f1e.png"
+  },
+  {
+    "revision": "a071b077f73b34503503c6f18975f60a",
+    "url": "/Portfolio/static/media/GraphQL.a071b077.png"
+  },
+  {
+    "revision": "1f0f3724598e4166fad36a2c2173fbe3",
+    "url": "/Portfolio/static/media/JS.1f0f3724.png"
+  },
+  {
+    "revision": "894c06a1e1ae6c2eae3177fe6d97e283",
+    "url": "/Portfolio/static/media/MoviesApp.894c06a1.png"
+  },
+  {
+    "revision": "824687bf51ddf785ff738d196f01359f",
+    "url": "/Portfolio/static/media/React.824687bf.png"
+  },
+  {
+    "revision": "a06de35baf0fe5fec924ce16f1baacda",
+    "url": "/Portfolio/static/media/ReactNative.a06de35b.png"
+  },
+  {
+    "revision": "53ecca5fb46f726ac918ee7caeaf2a7a",
+    "url": "/Portfolio/static/media/RestaurantFinder.53ecca5f.png"
+  },
+  {
+    "revision": "9f6f69696e85b1a93aa3dafc61d0937e",
+    "url": "/Portfolio/static/media/Sourcerer.9f6f6969.svg"
+  },
+  {
+    "revision": "2878be5f1c34daf5398f8f5ddba6038b",
+    "url": "/Portfolio/static/media/SultansKitchen.2878be5f.PNG"
+  },
+  {
+    "revision": "bc7296be0300805adf50594dcba208aa",
+    "url": "/Portfolio/static/media/artworx.bc7296be.png"
+  },
+  {
+    "revision": "89ec32967c2ed61368841a2f1717d69d",
+    "url": "/Portfolio/static/media/flutter.89ec3296.png"
+  },
+  {
+    "revision": "ba3c2a76f604639b73771b35e8fc2e32",
+    "url": "/Portfolio/static/media/groceristar.ba3c2a76.PNG"
+  },
+  {
+    "revision": "8648c71ee050c7fe690e42aa97b344f1",
+    "url": "/Portfolio/static/media/header.8648c71e.jpg"
+  },
+  {
+    "revision": "3eac64ffa1569cbf34a4ed20c7eac092",
+    "url": "/Portfolio/static/media/phone-laptop.3eac64ff.png"
+  },
+  {
+    "revision": "d6e24accc2c69be95a2913b52089ea2d",
+    "url": "/Portfolio/static/media/rdcsi.d6e24acc.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Portfolio/precache-manifest.c8f584e9f5b100fdba46552e22ffb2bb.js"
+  "/Portfolio/precache-manifest.2f95169243dabf84bff3834eb90e58d3.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/Portfolio/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
