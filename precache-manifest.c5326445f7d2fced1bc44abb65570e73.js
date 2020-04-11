a474ca20ea04027479dc14e19162ce449a628c46
smob123 <smob123@hotmail.com>
2020-04-11 23:04:27 +1200

    Updates

---
+++ precache-manifest.c5326445f7d2fced1bc44abb65570e73.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66bf9f9fe5ca193cbb14f6c25e9871d5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cb23fcc1c971fe86b59a",
+    "url": "/static/css/main.4d7295c3.chunk.css"
+  },
+  {
+    "revision": "2ca8a84842bd47b577c2",
+    "url": "/static/js/2.bd52edbf.chunk.js"
+  },
+  {
+    "revision": "4fdc00fbc861a978a4476cc8b3c4086f",
+    "url": "/static/js/2.bd52edbf.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cb23fcc1c971fe86b59a",
+    "url": "/static/js/main.c5ccbf93.chunk.js"
+  },
+  {
+    "revision": "00ccf1beb4d435cfbe60",
+    "url": "/static/js/runtime-main.c7cc6e23.js"
+  },
+  {
+    "revision": "bdba4f1ee1508a23e52eb780d6d999cb",
+    "url": "/static/media/Gatsby.bdba4f1e.png"
+  },
+  {
+    "revision": "a071b077f73b34503503c6f18975f60a",
+    "url": "/static/media/GraphQL.a071b077.png"
+  },
+  {
+    "revision": "1f0f3724598e4166fad36a2c2173fbe3",
+    "url": "/static/media/JS.1f0f3724.png"
+  },
+  {
+    "revision": "16aac88e1754612f4f5868fc6aee604a",
+    "url": "/static/media/Java.16aac88e.png"
+  },
+  {
+    "revision": "3ed9575dcc488c3e3a5bd66620bdf5a4",
+    "url": "/static/media/OpenSans-Regular.3ed9575d.ttf"
+  },
+  {
+    "revision": "6b78c7ec468eb0e13c6c5c4c39203986",
+    "url": "/static/media/Poppins-ExtraBold.6b78c7ec.ttf"
+  },
+  {
+    "revision": "41e8dead03fb979ecc23b8dfb0fef627",
+    "url": "/static/media/Poppins-Regular.41e8dead.ttf"
+  },
+  {
+    "revision": "824687bf51ddf785ff738d196f01359f",
+    "url": "/static/media/React.824687bf.png"
+  },
+  {
+    "revision": "a06de35baf0fe5fec924ce16f1baacda",
+    "url": "/static/media/ReactNative.a06de35b.png"
+  },
+  {
+    "revision": "bc7296be0300805adf50594dcba208aa",
+    "url": "/static/media/artworx.bc7296be.png"
+  },
+  {
+    "revision": "05acca4ff6b29fe838873a159b333e52",
+    "url": "/static/media/background.05acca4f.jpg"
+  },
+  {
+    "revision": "ba3c2a76f604639b73771b35e8fc2e32",
+    "url": "/static/media/groceristar.ba3c2a76.PNG"
+  },
+  {
+    "revision": "894c06a1e1ae6c2eae3177fe6d97e283",
+    "url": "/static/media/moviesApp.894c06a1.png"
+  },
+  {
+    "revision": "8ef40868db663235e0b8fff789e3f524",
+    "url": "/static/media/photorama.8ef40868.png"
+  },
+  {
+    "revision": "91d7a308b7907d31487148f91cc4f4de",
+    "url": "/static/media/profile-img.91d7a308.jpg"
+  },
+  {
+    "revision": "d6e24accc2c69be95a2913b52089ea2d",
+    "url": "/static/media/rdcsi.d6e24acc.jpg"
+  },
+  {
+    "revision": "53ecca5fb46f726ac918ee7caeaf2a7a",
+    "url": "/static/media/restaurantFinder.53ecca5f.png"
+  },
+  {
+    "revision": "2878be5f1c34daf5398f8f5ddba6038b",
+    "url": "/static/media/sultansKitchen.2878be5f.PNG"
+  }
+]);
