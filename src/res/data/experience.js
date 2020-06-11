2c86549e3073e3a1716e0921fcbe05eaedea9a3e
smob123 <smob123@hotmail.com>
2020-06-11 21:13:41 +1200

    updated work experience

--- src/res/data/experience.js
+++ src/res/data/experience.js
@@ -2,21 +2,27 @@
  * work experience, and education data.
  */
 
-const work = [
-    {
-        title: 'Front-end Developer',
+const work = [{
+        title: 'Front-end Developer (Remote Intern)',
         date: '05/2018 - 07/2018',
-        desc: 'Worked as a remote front-end web developer intern at Groceristar.'
+        desc: "Worked as a remote front-end web developer at Groceristar, where I participated in upgrading the company's website by developing a number of templates for the website's landing page, " +
+            "and taking a part in improving pre existing code."
     },
     {
-        title: 'Mobile Developer',
+        title: 'React Native Developer (Course Work)',
         date: '05/2018 - 08/2018',
-        desc: 'Worked as a React Native developer for Romeo Dallaire Child Soldiers Initiative.'
+        desc: "Worked as a React Native developer for Romeo Dallaire Child Soldiers Initiative, where I contributed to the implementation of a radar chart for a hybrid mobile application alongside team members, " +
+            "who have different coding experiences."
+    },
+    {
+        title: 'Frontend Web Developer (Volunteer)',
+        date: '03/2020 - Present',
+        desc: "Currently volunteering at Open Law NZ, where I am responsible for implementing new features and fixing existing bugs, as well as, participating in upgrading the website by implementing a new design, " +
+            "and adding new content."
     }
 ];
 
-const education = [
-    {
+const education = [{
         title: 'English Proficiency Diploma',
         date: '2014 - 2015',
         desc: 'VanWest College.'
